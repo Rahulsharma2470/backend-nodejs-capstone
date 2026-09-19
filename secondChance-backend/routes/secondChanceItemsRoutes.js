c5cb19c07b0bf1c29ba37fb43e9e8759ddddc86c
Rahulsharma2470 <rahulsharmajatuskern@gmail.com>
2026-09-19 15:48:28 +0530

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -44,9 +44,9 @@ router.post('/', upload.single('file'), async (req, res, next) => {
     await lastItemQuery.forEach(item => {
       secondChanceItem.id = (parseInt(item.id) + 1).toString()
     })
-
+    
    const dateAdded = Math.floor(new Date().getTime() / 1000)
-   secondChanceItem.date_added = dateAdded
+    secondChanceItem.date_added = dateAdded
 
     secondChanceItem = await collection.insertOne(secondChanceItem)
     res.status(201).json(secondChanceItem.ops[0])
